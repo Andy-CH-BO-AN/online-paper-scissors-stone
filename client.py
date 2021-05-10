fa9732d7461a4f47c2780edc6cc8de46f6262bb8
andy <andypig9@gmail.com>
2021-05-10 23:05:22 +0800

    update paper scissors stone

--- client.py
+++ client.py
@@ -1,39 +1,30 @@
 import pygame
 from network import Network
 from player import Player
+pygame.font.init()
 
-width = 500
-height = 500
+width = 700
+height = 700
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
 
+class Button:
+    def __init__(self, text , x, y, color):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 150
+        self.height = 100
 
-
-def redrawWindow(win, player, player2):
-    win.fill((255, 255, 255))
-    player.draw(win)
-    player2.draw(win)
-    pygame.display.update()
+def redrawWindow(win, game, p):
+    win.fill((128, 128, 128))
+    pass
 
 
 def main():
-    run = True
-    n = Network()
-    p = n.getP()
-    clock = pygame.time.Clock()
-
-    while run:
-        clock.tick(60)
-        p2 = n.send(p)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-
-        p.move()
-        redrawWindow(win, p, p2)
+    pass
 
 
 main()
--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import pickle
 import socket
 from _thread import *
 from player import Player
+from game import Game
+
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 server = IPAddr
@@ -14,45 +16,64 @@ try:
 except socket.error as e:
     str(e)
 
-s.listen(2)
+s.listen()
 print("Waiting for a connection, Server Started")
 
+connected = set()
+games = {}
+idCount = 0
 
-players = [Player(0,0,50,50,(255,0,0)),Player(0,0,50,50,(0,0,255))]
 
+def threaded_client(conn, p, gameID):
+    global idCount
+    conn.send(str.encode(str(p)))
 
-def threaded_client(conn, player):
-    conn.send(pickle.dumps(players[player]))
-    reply = ""
+    reply = ''
     while True:
         try:
-            data = pickle.loads(conn.recv(2048))
-            players[player] = data
+            data = conn.recv(4096).decode()
 
-            if not data:
-                print("Disconnected")
-                break
-            else:
-                if player == 1:
-                    reply = players[0]
+            if gameID in games:
+                game = games[gameId]
+
+                if not data:
+                    break
                 else:
-                    reply = players[1]
+                    if data == "reset":
+                        game.reset()
+                    elif data != "get":
+                        game.play(p, data)
 
-                print("Received: ", data)
-                print("Sending : ", reply)
-
-            conn.sendall(pickle.dumps(reply))
+                    reply = game
+                    conn.sendall(pickle.dumps(reply))
+            else:
+                break
         except:
             break
 
-    print("Lost connection")
+    print('Lost connection')
+
+    try:
+        del games[gameId]
+        print("Closing Game", gameID)
+    except:
+        pass
+    idCount -= 1
     conn.close()
 
 
-currentPlayer = 0
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
 
-    start_new_thread(threaded_client, (conn, currentPlayer))
-    currentPlayer += 1
+    idCount += 1
+    p = 0
+    gameId = (idCount - 1) // 2
+    if idCount % 2 == 1:
+        games[gameId] = Game[gameId]
+        print('Creating a new game...')
+    else:
+        games[gameId].ready = True
+        p = 1
+
+    start_new_thread(threaded_client, (conn, p, gameId))
