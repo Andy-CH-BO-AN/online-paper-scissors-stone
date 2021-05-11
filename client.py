f28e8cda07e51ebb4bf73f0d2b50c7af953d759d
帥哥 <andre.chen@shoalter.com>
2021-05-11 17:26:03 +0800

    drawling

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import pygame
 from network import Network
 from player import Player
+
 pygame.font.init()
 
 width = 700
@@ -10,13 +11,21 @@ pygame.display.set_caption("Client")
 
 
 class Button:
-    def __init__(self, text , x, y, color):
+    def __init__(self, text, x, y, color):
         self.text = text
         self.x = x
         self.y = y
         self.color = color
         self.width = 150
         self.height = 100
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("comicsans", 40)
+        text = font.render(self.text, 1, (255, 255, 255))
+        win.blit(text, (self.x + round(self.width / 2) - round(text.get_width() / 2)),
+                 self.y + round(self.height / 2) - round(text.get_height() / 2))
+
 
 def redrawWindow(win, game, p):
     win.fill((128, 128, 128))
